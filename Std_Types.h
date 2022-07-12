b93e346c383523b083ec467929bb2fad79073015
mohab2338 <mohab2338@gmail.com>
2022-07-12 18:33:12 +0200

    added std_Types platform_Types DIO.h DIO.c PIN Configuration only not all the Port

---
+++ Std_Types.h
@@ -0,0 +1,39 @@
+/**************************************************
+ *	FILE DESCRIPTION
+ * ------------------------------------------------
+ *		File: Std_Types.h
+ *
+ *
+ *
+ *		Description: contains Standard Types
+ *
+ *
+ **************************************************/
+ #ifndef STD_TYPES_H
+ #define STD_TYPES_H
+ /*************************************************
+ * INCLUDES
+ **************************************************/
+ #include "Platform_Types.h"
+ /*************************************************
+ * GLOBAL MACROS
+ *************************************************/
+ #define TRUE   			1
+ #define FALSE  			0
+ 
+ #define ENABLE 			1
+ #define DISABLE 			0
+ 
+ #define STD_HIGH   	1
+ #define STD_LOW			0
+ 
+ #define STD_ACTIVE	 	1
+ #define STD_IDLE			0
+ 
+ #define STD_ON				1
+ #define STD_OFF	   	0
+ 
+ 
+ 
+ 
+#endif /*STD_TYPES_H*/ 
